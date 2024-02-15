7b67140c4ba322b70b83d5babc7d0d39e02168aa
ursa226 <ursa226@gmail.com>
2024-02-15 19:08:33 +0800

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+<body>
+<h1>Hello, world!</h1>
+
+<!-- Jquery cdn -->
+<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+<!--Bootstrap js-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
